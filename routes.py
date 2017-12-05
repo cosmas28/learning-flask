c06e30028391771b5c68bd9132b0430cd3faa9b0
cosmas28 <augubilla100@gmail.com>
2017-12-05 18:16:38 +0300

    add form integration in routes file

--- routes.py
+++ routes.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
 from models import db
+from forms import SignupForm
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
 db.init_app(app)
+
+app.secret_key = "development-key"
 
 @app.route("/")
 def index():
@@ -12,6 +15,11 @@ def index():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app_route("/signup", methods=["GET", "GOT"])
+def signup():
+    form = SignupForm()
+    return render_template('signup.html', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
